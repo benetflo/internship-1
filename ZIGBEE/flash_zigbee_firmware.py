17c5680a03ef997f9eda7b804130d589af75285e
Benji <ben.flodin@outlook.com>
2025-10-21 13:45:59 +0200

    Implement ZIP extraction for firmware directory
    Added functionality to unzip firmware if LOCAL_GBL_DIR is a ZIP file.

--- ZIGBEE/flash_zigbee_firmware.py
+++ ZIGBEE/flash_zigbee_firmware.py
@@ -2,8 +2,11 @@ import subprocess
 import sys
 from pathlib import PureWindowsPath
 import os
+import zipfile
 
-LOCAL_GBL_DIR = PureWindowsPath('c:/users/benfl/downloads/firmware-build-hemla_zigbee_ncp_7.5.0.0')
+
+LOCAL_GBL_DIR = PureWindowsPath('c:/users/benfl/downloads/firmware-build-hemla_zigbee_ncp_7.5.0.0') # THIS CAN BE A ZIPPED FOLDER, PROGRAM WILL UNZIP IF NEEDED
+UNZIPPED_DIR = "c:/users/benfl/downloads/unzipped_firmware" # created and used if user enters ZIP-file as LOCAL_GBL_DIR
 REMOTE_GBL_PATH = "~/ZIGBEE"
 SSH_HOST = "benflo@192.168.1.198"
 DEVICE_PATH = "/dev/ttyAMA3"
@@ -31,7 +34,16 @@ def get_last_modified(gbls: dict):
             
 def find_latest_gbl():
             
-    # UNZIP FIRST ??? 
+    global LOCAL_GBL_DIR
+    # UNZIP FOLDER IF NEEDED
+    if os.path.isfile(str(LOCAL_GBL_DIR)) and str(LOCAL_GBL_DIR).lower().endswith(".zip"):
+        
+        if not os.path.exists(UNZIPPED_DIR):
+            os.makedirs(UNZIPPED_DIR)
+        with zipfile.ZipFile(str(LOCAL_GBL_DIR), 'r') as zip_ref:
+            zip_ref.extractall(UNZIPPED_DIR)
+        print(f"ZIP extracted to: {UNZIPPED_DIR}")
+        LOCAL_GBL_DIR = UNZIPPED_DIR 
 
     dir_list = os.listdir(LOCAL_GBL_DIR)
     gbl_dict = {}
