5d5cfad1a8474456c27aded9e5ef754d0d19fc83
Benji <ben.flodin@outlook.com>
2025-10-21 14:23:10 +0200

    Update flash_zigbee_firmware.py

--- ZIGBEE/flash_zigbee_firmware.py
+++ ZIGBEE/flash_zigbee_firmware.py
@@ -4,14 +4,14 @@ from pathlib import PureWindowsPath
 import os
 import zipfile
 
+# fixa ssh nycklar? 
 
-LOCAL_GBL_DIR = PureWindowsPath('c:/users/benfl/downloads/firmware-build-hemla_zigbee_ncp_7.5.0.0') # THIS CAN BE A ZIPPED FOLDER, PROGRAM WILL UNZIP IF NEEDED
-UNZIPPED_DIR = "c:/users/benfl/downloads/unzipped_firmware" # created and used if user enters ZIP-file as LOCAL_GBL_DIR
+LOCAL_GBL_DIR = PureWindowsPath('c:/users/benfl/downloads/firmware-build-hemla_zigbee_ncp_7.5.0.0') # THIS SHOULD BE A FOLDER, IT CAN BE A ZIPPED FOLDER, PROGRAM WILL UNZIP IF NEEDED
+UNZIPPED_DIR = "c:/users/benfl/downloads/unzipped_firmware" # created if user enters ZIP-file as LOCAL_GBL_DIR. LOCAL_GBL_DIR will be set to this after unzipping
 REMOTE_GBL_PATH = "~/ZIGBEE"
 SSH_HOST = "benflo@192.168.1.198"
-DEVICE_PATH = "/dev/ttyAMA3"
-VIRTUAL_ENV_NAME = ".benjamin-zigbee"
-BAUDRATE = 115200
+DEVICE_PATH = "/dev/ttyAMA3"                # device port of the Zigbee chip
+VIRTUAL_ENV_NAME = ".benjamin-zigbee"       # name of python venv on CM5
 
 def get_last_modified(gbls: dict):
     
@@ -35,6 +35,7 @@ def get_last_modified(gbls: dict):
 def find_latest_gbl():
             
     global LOCAL_GBL_DIR
+
     # UNZIP FOLDER IF NEEDED
     if os.path.isfile(str(LOCAL_GBL_DIR)) and str(LOCAL_GBL_DIR).lower().endswith(".zip"):
         
@@ -45,25 +46,33 @@ def find_latest_gbl():
         print(f"ZIP extracted to: {UNZIPPED_DIR}")
         LOCAL_GBL_DIR = UNZIPPED_DIR 
 
+    # IF LOCAL_GBL_DIR is not a folder or not found
+    elif not os.path.isdir(str(LOCAL_GBL_DIR)):
+        sys.exit(f"{LOCAL_GBL_DIR} - DOES NOT EXIST OR IS NOT A DIRECTORY")
+
     dir_list = os.listdir(LOCAL_GBL_DIR)
-    gbl_dict = {}
     latest_gbl_file = ""
+    list_of_gbls = []
     
+    # add all present .gbl files to list
     for file in dir_list:
         if file.endswith(".gbl"):
-            gbl_dict["file_name"] = file
             modified_time = os.path.getmtime(f"{LOCAL_GBL_DIR}/{file}")    
-            gbl_dict["last_modified"] = modified_time
+            list_of_gbls.append({
+                "file_name": file,
+                "last_modified": modified_time
+            })
     
     # FILE NOT FOUND --> Clean exit
-    if gbl_dict == {}:
+    if list_of_gbls == []:
         sys.exit(f"FILE NOT FOUND IN --> {LOCAL_GBL_DIR}")
     
-    latest_gbl_file = get_last_modified(gbl_dict)
+    # get latest modified firmware file
+    latest_gbl_file = max(list_of_gbls, key=lambda x: x["last_modified"])["file_name"]
 
     return latest_gbl_file
 
-def upload_to_cm5(local_file):
+def upload_to_cm5(local_file: str):
 
     print() # this is just for clean print in terminal
 
@@ -73,17 +82,17 @@ def upload_to_cm5(local_file):
     if result.returncode == 0:
         print("\nUpload to CM5 was successful! :)")
     else:
-        print("\nUpload to CM5 was unsuccessful! :()")
+        print("\nUpload to CM5 was unsuccessful! :(")
 
-def flash_on_cm5(local_file):
+def flash_on_cm5(local_file: str):
     
     command = f"cd ZIGBEE && source {VIRTUAL_ENV_NAME}/bin/activate && universal-silabs-flasher --device {DEVICE_PATH} flash --firmware {REMOTE_GBL_PATH}/{local_file} && universal-silabs-flasher --device {DEVICE_PATH} probe"
     # Has to enter password again :(
-    result = subprocess.run(["ssh", "-t", f"{SSH_HOST}", command], check=False)
-    # SHOW OUTPUT?
+    result = subprocess.run(["ssh", "-t", f"{SSH_HOST}", command], check=False, stderr=subprocess.PIPE, text=True)
 
+    # if successful
     if result.returncode == 0:
-        return True
+        return True 
     
     return False
 
@@ -92,11 +101,13 @@ def run_reset_chip():
     print("Running 'reset_chip.py'...")
     command = f"cd ZIGBEE && sudo python reset_chip.py" 
     # Has to enter password again :)
-    result = subprocess.run(["ssh", "-t", f"{SSH_HOST}", command], check=False)
+    result = subprocess.run(["ssh", "-t", f"{SSH_HOST}", command], check=False, stderr=subprocess.PIPE, text=True)
 
+    # if successful
     if result.returncode == 0:
         return True
     
+    print(result.stderr)
     return False
 
 def main():
