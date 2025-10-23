5a5bed922829883f10767d2800a9cf2ed5494a24
benetflo <ben.flodin@outlook.com>
2025-10-23 13:19:42 +0200

    minor changes

--- Orelaterat/run_len_encoding/bin_files/main.c
+++ Orelaterat/run_len_encoding/bin_files/main.c
@@ -26,17 +26,15 @@ void check_dir()
     }
 }
 
-
 int main()
 {
     //check if output dir exists otherwise create it
     check_dir();
     
+    FILE* pBinFile = fopen(old_file_path, RD_BIN_ONLY);
+    FILE* pNewBinFile = fopen(new_file_path, WR_BIN_ONLY);
     
-    FILE* pMP3 = fopen(old_file_path, RD_BIN_ONLY);
-    FILE* pNewMP3 = fopen(new_file_path, WR_BIN_ONLY);
-    
-    if(pMP3 == NULL)
+    if(pBinFile == NULL)
     {
         printf("Error %d, from fopen: %s\n", errno, strerror(errno));
         exit(1);
@@ -48,7 +46,7 @@ int main()
     unsigned char curr_byte = 0;
     uint8_t counter = 0;
 
-    while((bytes_read = fread(buffer, 1, BUFFER_SIZE, pMP3)) > 0)
+    while((bytes_read = fread(buffer, 1, BUFFER_SIZE, pBinFile)) > 0)
     {
         
         for(size_t i = 0; i < bytes_read; i++)
@@ -65,16 +63,16 @@ int main()
                 counter++;
                 if(counter == 255)
                 {
-                    fwrite(&curr_byte, 1, 1, pNewMP3);
-                    fwrite(&counter, 1, 1, pNewMP3);
+                    fwrite(&curr_byte, 1, 1, pNewBinFile);
+                    fwrite(&counter, 1, 1, pNewBinFile);
                     counter = 0;
                 }
             }else
             {
                 if(counter > 0)
                 {
-                    fwrite(&curr_byte, 1, 1, pNewMP3);
-                    fwrite(&counter, 1, 1, pNewMP3);
+                    fwrite(&curr_byte, 1, 1, pNewBinFile);
+                    fwrite(&counter, 1, 1, pNewBinFile);
                 }
                 curr_byte = b;
                 counter = 1;
@@ -83,12 +81,12 @@ int main()
     }
     if(counter > 0)
     {
-        fwrite(&curr_byte, 1, 1, pNewMP3);
-        fwrite(&counter, 1, 1, pNewMP3);
+        fwrite(&curr_byte, 1, 1, pNewBinFile);
+        fwrite(&counter, 1, 1, pNewBinFile);
     }
 
-    fclose(pMP3);
-    fclose(pNewMP3);
+    fclose(pBinFile);
+    fclose(pNewBinFile);
 
     return 0;
 }
